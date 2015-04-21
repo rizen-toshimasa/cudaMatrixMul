a2a661b045a9caa232c85941c8f75b2d9769cc76
toshimasa <toshimasa@toshimasa-Vostro-420-Series.(none)>
2015-04-21 23:00:42 +0900

    関数や変数名変更

--- MM.cu
+++ MM.cu
@@ -1,188 +1,191 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-//#include <cblas.h>//cuda用線形計算ライブラリ 
 #define M_SIZE 1024//mat size
-#define B_SIZE 512//block size
-//CPU プロトタイプ
-void matMul(int *HM1, int *HM2, int *HM3);
-void matZeros(int *HM);
-void matTranspose(int *iMat, int*oMat);
-int matDiffCount(int *HM1, int *HM2);
-void printHM(int *HM);
-//CUDA プロトタイプ
-__global__ void cudaMatrixMul(int *GM1, int *GM2, int *GM3);
-__global__ void cudaMatrixMulShared(int *GM1, int *GM2, int *GM3);
-__global__ void cudaMatrixMulSharedTsukuba(int *GM1, int *GM2, int *GM3);
-//メイン関数
-int main(void){
+#define B_SIZE 1024//block size
+//������
+void matMul(int *mat1, int *mat2, int *dstMat);
+void matZeros(int *mat);
+void matTrans(int *mat, int *dstMat);
+int matDiffCount(int *mat1, int *mat2);
+void printMat(int *mat);
+//CUDA������
+__global__ void cudaMatMul(int *mat1, int *mat2, int *dstMat);
+__global__ void cudaMatMulShared(int *mat1, int *mat2, int *dstMat);
+__global__ void cudaMatMulSharedTsukuba(int *mat1, int *mat2, int *dstMat);
+//�����
+int main(int argc, char* argv[]){
     srand((unsigned)time(NULL));
-    //Host Memory に Matrix 確保 HM1 = HM2 * HM3
-    int *HM1 = (int *)malloc(sizeof(int) * M_SIZE * M_SIZE);
-    int *HM2 = (int *)malloc(sizeof(int) * M_SIZE * M_SIZE);
-    int *HM3 = (int *)malloc(sizeof(int) * M_SIZE * M_SIZE);
-    //Host Memoryにデータ格納
-    matZeros(HM1);
+    //Host Memory
+    int *mat1 = (int *)malloc(sizeof(int) * M_SIZE * M_SIZE);
+    int *mat2 = (int *)malloc(sizeof(int) * M_SIZE * M_SIZE);
+    int *matResult = (int *)malloc(sizeof(int) * M_SIZE * M_SIZE);
+    //Host Memory
+    matZeros(matResult);
     for(int i=0; i<M_SIZE*M_SIZE; i++){
-        HM2[i] = rand()%256;
-        HM3[i] = rand()%256;
+        mat1[i] = rand()%256;
+        mat2[i] = rand()%256;
     }
-    //Global Memory 確保
-    int *GM1,*GM2,*GM3;
-    cudaMalloc((void **)&GM1, sizeof(int) * M_SIZE * M_SIZE);
-    cudaMalloc((void **)&GM2, sizeof(int) * M_SIZE * M_SIZE);
-    cudaMalloc((void **)&GM3, sizeof(int) * M_SIZE * M_SIZE);
-    //GlobalMemoryにデータ格納
-    cudaMemcpy(GM1, HM1, sizeof(int) * M_SIZE * M_SIZE, cudaMemcpyHostToDevice);
-    cudaMemcpy(GM2, HM2, sizeof(int) * M_SIZE * M_SIZE, cudaMemcpyHostToDevice);
-    cudaMemcpy(GM3, HM3, sizeof(int) * M_SIZE * M_SIZE, cudaMemcpyHostToDevice);
-    //CPUでの計算
+    //Global Memory
+    int *gMat1,*gMat2,*gMatResult;
+    cudaMalloc((void **)&gMat1, sizeof(int) * M_SIZE * M_SIZE);
+    cudaMalloc((void **)&gMat2, sizeof(int) * M_SIZE * M_SIZE);
+    cudaMalloc((void **)&gMatResult, sizeof(int) * M_SIZE * M_SIZE);
+    //GlobalMemory
+    cudaMemcpy(gMat1, mat1, sizeof(int) * M_SIZE * M_SIZE, cudaMemcpyHostToDevice);
+    cudaMemcpy(gMat2, mat2, sizeof(int) * M_SIZE * M_SIZE, cudaMemcpyHostToDevice);
+    cudaMemcpy(gMatResult, matResult, sizeof(int) * M_SIZE * M_SIZE, cudaMemcpyHostToDevice);
+    //CPU
     cudaEvent_t cpuStartTime,cpuStopTime;
     float cpuTime;
     cudaEventCreate(&cpuStartTime);
     cudaEventCreate(&cpuStopTime);
     cudaEventRecord(cpuStartTime, 0);
-    matMul(HM1, HM2, HM3);
+    matMul(mat1, mat2, matResult);
     cudaEventRecord(cpuStopTime, 0);
     cudaEventSynchronize(cpuStopTime);
     cudaEventElapsedTime(&cpuTime, cpuStartTime, cpuStopTime);
-    //CUDAでの計算
+    //CUDA
     cudaEvent_t cudaStartTime, cudaStopTime;
     float cudaTime;
-    dim3 Dg(M_SIZE*M_SIZE/B_SIZE, 1, 1), Db(B_SIZE, 1, 1);
+    dim3 Dg(M_SIZE*M_SIZE/B_SIZE, 1, 0), Db(B_SIZE, 1, 1);
     cudaEventCreate(&cudaStartTime);
     cudaEventCreate(&cudaStopTime);
     cudaEventRecord(cudaStartTime, 0);
-    cudaMatrixMulSharedTsukuba <<< Dg, Db>>> (GM1, GM2, GM3);
+    cudaMatMulSharedTsukuba <<< Dg, Db>>> (gMat1, gMat2, gMatResult);
     cudaEventRecord(cudaStopTime, 0);
     cudaEventSynchronize(cudaStopTime);
     cudaEventElapsedTime(&cudaTime, cudaStartTime, cudaStopTime);
-    //Cuda計算結果をHostMemoryにコピー
-    int *cudaHM1 = (int *)malloc(sizeof(int) * M_SIZE * M_SIZE);
-    int *cudaHM2 = (int *)malloc(sizeof(int) * M_SIZE * M_SIZE);
-    int *cudaHM3 = (int *)malloc(sizeof(int) * M_SIZE * M_SIZE);
-    cudaMemcpy(cudaHM1, GM1, sizeof(int) * M_SIZE * M_SIZE, cudaMemcpyDeviceToHost);
-    cudaMemcpy(cudaHM2, GM2, sizeof(int) * M_SIZE * M_SIZE, cudaMemcpyDeviceToHost);
-    cudaMemcpy(cudaHM3, GM3, sizeof(int) * M_SIZE * M_SIZE, cudaMemcpyDeviceToHost);
-    //標準出力
+    //Cuda
+    int *cudamat1 = (int *)malloc(sizeof(int) * M_SIZE * M_SIZE);
+    int *cudamat2 = (int *)malloc(sizeof(int) * M_SIZE * M_SIZE);
+    int *cudamat3 = (int *)malloc(sizeof(int) * M_SIZE * M_SIZE);
+    cudaMemcpy(cudamat1, gMat1, sizeof(int) * M_SIZE * M_SIZE, cudaMemcpyDeviceToHost);
+    cudaMemcpy(cudamat2, gMat2, sizeof(int) * M_SIZE * M_SIZE, cudaMemcpyDeviceToHost);
+    cudaMemcpy(cudamat3, gMatResult, sizeof(int) * M_SIZE * M_SIZE, cudaMemcpyDeviceToHost);
+    //��
     printf("M_SIZE:%d\n",M_SIZE);
-    if(matDiffCount(HM1,cudaHM1)){
-        puts("CPUとGPUの計算結果は一致しました");
+    if(matDiffCount(mat1,cudamat1)){
+        puts("CPU��GPU�η׻���̤ϰ��פ��ޤ���");
     }else{
-        puts("CPUとGPUの計算結果は一致しませんでした");
+        puts("CPU��GPU�η׻���̤ϰ��פ��ޤ���Ǥ���");
     }
     if(M_SIZE < 10){
         puts("M1");
-        printHM(HM1);
+        printMat(mat1);
         puts("M2");
-        printHM(HM2);
+        printMat(mat2);
         puts("M3");
-        printHM(HM3);
+        printMat(matResult);
         puts("cudaM");
-        printHM(cudaHM1);
+        printMat(cudamat1);
         puts("cuda2");
-        printHM(cudaHM2);
+        printMat(cudamat2);
         puts("cuda3");
-        printHM(cudaHM3);
+        printMat(cudamat3);
     }
     printf("CPU:Time = %f\n", cpuTime);
     printf("GPU:Time = %f\n", cudaTime);
-    //Host Memory開放
-    free(HM1);
-    free(HM2);
-    free(HM3);
-    //Global Memory開放
-    cudaFree(GM1);
-    cudaFree(GM2);
-    cudaFree(GM3);
+    //Host Memory
+    free(mat1);
+    free(mat2);
+    free(matResult);
+    //Global Memory
+    cudaFree(gMat1);
+    cudaFree(gMat2);
+    cudaFree(gMatResult);
     return 0;
 }
-//CUDA版行列の積
-__global__ void cudaMatrixMul(int *GM1, int *GM2, int *GM3){
+//CPU
+void matMul(int *mat1, int *mat2, int *dstMat){
+    int transMat[M_SIZE*M_SIZE];
+    matZeros(transMat);
+    matTrans(mat2,transMat);//
+    for(int i=0; i<M_SIZE; i++){
+        for(int j=0; j<M_SIZE; j++){
+            for(int k=0; k<M_SIZE; k++){
+                dstMat[i*M_SIZE + j] += mat1[i*M_SIZE + k] * transMat[j*M_SIZE + k];
+            }
+        }
+    }
+}
+//CUDA
+__global__ void cudaMatMul(int *mat1, int *mat2, int *dstMat){
     int id = blockDim.x * blockIdx.x + threadIdx.x;
     int row = id/M_SIZE;
     int column = id%M_SIZE;
     int x=0;
     for(int i=0; i<M_SIZE; i++){
-        x += GM2[row*M_SIZE+i] * GM3[i*M_SIZE+column];
+        x += mat1[row*M_SIZE+i] * mat2[i*M_SIZE+column];
     }
-    GM1[id] = x;
+    dstMat[id] = x;
 }
-//CUDA,SharedMemory使用版行列の積
-__global__ void cudaMatrixMulShared(int *GM1, int *GM2, int *GM3){
-    __shared__ int SM1[1024], SM2[1024], SM3[1024];
+//CUDA,SharedMemory
+__global__ void cudaMatMulShared(int *mat1, int *mat2, int *dstMat){
+    __shared__ int sMat1[1024], sMat2[1024];
     unsigned int tid = threadIdx.x;
     unsigned int id = blockIdx.x*blockDim.x + threadIdx.x;
-    SM1[tid] = GM1[id];
-    SM2[tid] = GM2[id];
-    SM3[tid] = GM3[id];
+    sMat1[tid] = mat1[id];
+    sMat2[tid] = mat2[id];
     __syncthreads();
     int row = id/M_SIZE;
     int column = id%M_SIZE;
     int x=0;
     for(int i=0; i<M_SIZE; i++){
-        x += SM2[row*M_SIZE+i] * SM3[i*M_SIZE+column];
+        x += sMat1[row*M_SIZE+i] * sMat2[i*M_SIZE+column];
     }
-    GM1[id] = x;
+    dstMat[id] = x;
 }
 //tsukuba,Shared
-__global__ void cudaMatrixMulSharedTsukuba(int *GM1, int *GM2, int *GM3){
-    __shared__ int SM2[1024],SM3[1024];
+__global__ void cudaMatMulSharedTsukuba(int *mat1, int *mat2, int *dstMat){
+    __shared__ int sMat1[1024],sMat2[1024];
     unsigned int tid = threadIdx.x;
     unsigned int id = blockIdx.x * blockDim.x + threadIdx.x;
-    //SM1[tid]     = GM1[id];
-    //SM1[tid+512] = GM1[id+512];
-    SM2[tid]     = GM2[id];
-    SM2[tid+512] = GM2[id+512];
-    SM3[tid]     = GM3[id];
-    SM3[tid+512] = GM3[id+512];
+    //SM1[tid]     = gMat1[id];
+    //SM1[tid+512] = gMat1[id+512];
+    sMat1[tid]     = mat1[id];
+    sMat1[tid+512] = mat1[id+512];
+    sMat2[tid]     = mat2[id];
+    sMat2[tid+512] = mat2[id+512];
     __syncthreads();
     int row = id/M_SIZE;
     int column = id %M_SIZE;
     int x = 0;
     for(int i=0; i<M_SIZE; i++){
-        x += SM2[row * M_SIZE + i] * SM3[i * M_SIZE + column];
+        x += sMat1[row * M_SIZE + i] * sMat2[i * M_SIZE + column];
     }
-    GM1[id] = x;
+    mat1[id] = x;
 }
-//CPU版行列の積
-void matMul(int *HM1, int *HM2, int *HM3){
-    int tmpHM[M_SIZE*M_SIZE];
-    matZeros(tmpHM);
-    matTranspose(HM3,tmpHM);//転置
+//
+void matZeros(int *mat){
+    for(int i=0; i<M_SIZE*M_SIZE; i++){
+        mat[i]=0;
+    }
+}
+//
+void matTrans(int *mat, int *dstMat){
     for(int i=0; i<M_SIZE; i++){
         for(int j=0; j<M_SIZE; j++){
-            for(int k=0; k<M_SIZE; k++){
-                HM1[i*M_SIZE + j] += HM2[i*M_SIZE + k] * tmpHM[j*M_SIZE + k];
-            }
+            dstMat[j*M_SIZE + i] = mat[i*M_SIZE + j];
         }
     }
 }
-void matZeros(int *HM){
+//
+int matDiffCount(int *mat1, int *mat2){
+    int count = 0;
     for(int i=0; i<M_SIZE*M_SIZE; i++){
-        HM[i]=0;
-    }
-}
-//転置行列
-void matTranspose(int *iMat, int *oMat){
-    for(int i=0; i<M_SIZE; i++){
-        for(int j=0; j<M_SIZE; j++){
-            oMat[j*M_SIZE + i] = iMat[i*M_SIZE + j];
-        }
-    }
-}
-int matDiffCount(int *HM1, int *HM2){
-    for(int i=0; i<M_SIZE*M_SIZE; i++){
-        if(HM1[i] - HM2[i]){
+        if(mat1[i] - mat2[i]){
             return 0;
+        }else{
+            count++;
         }
     }
-    return 1;
+    return count;
 }
-void printHM(int *HM){
+void printMat(int *mat){
     for(int i=0; i<M_SIZE; i++){
         for(int j=0; j<M_SIZE;j++){
-            printf("%d,",HM[i*M_SIZE + j]);
+            printf("%d,",mat[i*M_SIZE + j]);
         }
         printf("\n");
     }
